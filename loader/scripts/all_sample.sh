615d84729960f5dab58b0976ed1d1b2880c1c657
tmushayahama <tmushayahama@gmail.com>
2022-11-22 15:08:19 -0800

    Added sample data and test-data

---
+++ loader/scripts/all_sample.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+annotaions=./data/test_data/sample_human_iba_annotations.json
+terms=./data/test_data/terms.json
+genes=./data/test_data/human_iba_gene_info.json
+clean_articles=./downloads/clean_articles2.json
+clean_annotations=./downloads/human_iba_annotations_clean_2.json
+
+
+python3 -m src.get_articles -a $annotaions -o $clean_articles
+
+python3 -m src.clean_annotations \
+-a $annotaions \
+-t $terms \
+-art $clean_articles \
+-g $genes \
+-o $clean_annotations
+
+python3 -m src.index_es -a $clean_annotations
