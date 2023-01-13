b4a4e60416b815cdedbbb27644875e0debbd7a80
dustine32 <dustine32@gmail.com>
2023-01-13 12:59:23 -0800

    Slim term of unknown is unknown, not other; for #4

--- data_conversion/iba_exp_refs_to_json.py
+++ data_conversion/iba_exp_refs_to_json.py
@@ -216,9 +216,8 @@ class IbaExpRefCollection:
                 for aspect, result in has_aspect.items():
                     if not result:
                         unknown_aspect_term = self.ontology_manager.UNKNOWN_TERMS[aspect]
-                        other_aspect_term = self.ontology_manager.OTHER_TERMS[aspect]
                         new_annot = self.create_annotation_for_gene(gene_id, gene_name, gene_symbol, unknown_aspect_term)
-                        new_annot["slim_terms"] = [other_aspect_term]
+                        new_annot["slim_terms"] = [unknown_aspect_term]
                         new_annot["group"] = "GO_Central"
                         self.annotation_lkp[gene_id][unknown_aspect_term] = {"": new_annot}
 
