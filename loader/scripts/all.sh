e4aa696378a70df5a6a663137a3e771f5b8f6210
tmushayahama <tmushayahama@gmail.com>
2023-01-31 21:06:03 -0800

    Added the evidence type n/a, stats and checks of gene id, symbol and label count

--- loader/scripts/all.sh
+++ loader/scripts/all.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-annotations=./downloads/human_iba_annotations.json
-terms=./downloads/terms.json
-genes=./downloads/human_iba_gene_info.json
-clean_articles=./downloads/clean_articles2.json
-clean_annotations=./downloads/human_iba_annotations_clean_2.json
+
+terms_fp='./downloads/input/full_go_annotated.json'
+annotations_fp='./downloads/input/human_iba_annotations.json'
+clean_articles_fp='./downloads/clean-articles.json'
+genes_fp='./downloads/input/human_iba_gene_info.json'
+taxon_fp='./downloads/input/taxon_lkp.json'
+clean_annotations_fp='./downloads/human_iba_annotations_clean2.json'
 
 
-python3 -m src.get_articles -a $annotations -o $clean_articles
+python3 -m src.get_articles -a $annotations_fp -o $clean_articles_fp
 
 python3 -m src.clean_annotations \
--a $annotations \
--t $terms \
--art $clean_articles \
--g $genes \
--o $clean_annotations
+-a $annotations_fp \
+-t $terms_fp \
+-art $clean_articles_fp \
+-g $genes_fp \
+-o $clean_annotations_fp
 
-python3 -m src.index_es -a $clean_annotations
+python3 -m src.index_es -a $clean_annotations_fp
--- loader/src/clean_annotations.py
+++ loader/src/clean_annotations.py
@@ -9,7 +9,8 @@ def main():
     parser = parse_arguments()
     terms_df = get_terms_map(parser.terms_fp)
     articles_df = get_articles_map(parser.articles_fp)
-    genes_df = get_genes_map(parser.genes_fp)
+    taxon_df = get_taxon_map(parser.taxon_fp)
+    genes_df = get_genes_map(parser.genes_fp, taxon_df)
     annos_df = get_annos(parser.annos_fp, terms_df, genes_df, articles_df)
     anno_json = annos_df.to_json(orient="records", default_handler=None)
     json_str = json.loads(anno_json)
@@ -44,34 +45,34 @@ def get_aspect(df, k):
     aspect = row.get('aspect', None)
     return aspect
 
-def get_evidence(df, gene_df, evidences):
+
+def get_evidence(df, genes_df, row):
     result = []
-    for evidence in evidences:
+    for evidence in row['evidence']:
         result_ref =[]
         for reference in evidence['references']:
-            row = get_pd_row_key(df, reference)
-            result_ref.append(row)
-        gene_row = get_pd_row_key(gene_df, evidence['with_gene_id'])
-        result.append({
-            'with_gene_id':gene_row,
-            'references':result_ref
-        })
+            result_ref.append(get_pd_row_key(df, reference))
+        gene_row = get_pd_row_key(genes_df, evidence['with_gene_id'])
+       
+        evidence_item = {
+          'with_gene_id':gene_row,
+          'group': evidence['group'],
+          'references':result_ref
+        }
+        result.append(evidence_item)
             
     return result
+
 
 def get_evidence_type(row):
     for evidence in row['evidence']:
         if evidence["with_gene_id"]['gene'] == row['gene']:
             return 'direct'
+        if len(evidence['references']) == 0:
+            return 'n/a' 
             
     return 'homology'
 
-def get_slim(row):
-    for evidence in row['evidence']:
-        if evidence["with_gene_id"]['gene'] == row['gene']:
-            return 'direct'
-            
-    return 'homology'
 
 # Terms
 def get_terms_map(terms_fp):
@@ -87,23 +88,50 @@ def get_articles_map(articles_fp):
     articles_df = articles_df.set_index('pmid', drop=False)
     return articles_df
 
+#Taxon
+def get_taxon_map(taxon_fp):
+    taxon_df = pd.read_json(taxon_fp, dtype={'taxon_id':str})
+    return taxon_df
+
+
 # Gene
-def get_genes_map(genes_fp):
-    genes_df = pd.read_json(genes_fp)
+def get_genes_map(genes_fp, taxon_df):
+    genes_df = pd.read_json(genes_fp, dtype={'taxon_id':str})
+    genes_df = genes_df.merge(taxon_df, how='left', on='taxon_id')
     genes_df = genes_df.set_index('gene', drop=False)
     return genes_df
+
+
+def count_evidence(evidences):
+    return len(evidences)
+
+
+def get_groups(evidences):
+    groups = set()
+    for evidence in evidences:
+        groups.add(evidence['group'])
+    return list(groups)
+
 
 def get_annos(annos_fp, terms_df, genes_df, articles_df):
 
     annos_df = pd.read_json(annos_fp)
+    annos_df = annos_df.merge(genes_df[
+        ['taxon_id', 
+        'taxon_label', 
+        'taxon_abbr' , 
+        'coordinates_chr_num',
+        'coordinates_start',
+        'coordinates_end',
+        'coordinates_strand']], how='left', left_on="gene", right_index=True)
     annos_df['aspect'] = annos_df['term'].apply(lambda x: get_pd_row(terms_df, x)['aspect'])
     annos_df['term'] = annos_df['term'].apply(lambda x: get_pd_row(terms_df, x))
     annos_df['slim_terms'] = annos_df['slim_terms'].apply(lambda x: spread_terms(terms_df, x))
-    annos_df['relation'] = annos_df['qualifiers'].apply(lambda x: x[0])
-    annos_df['relation'] = annos_df['relation'].str.replace('_', ' ')
-    annos_df['evidence'] = annos_df['evidence'].apply(lambda x: get_evidence(articles_df, genes_df, x))
+    annos_df['qualifier'] = annos_df['qualifier'].str.replace('_', ' ')
+    annos_df['evidence'] = annos_df.apply(lambda x: get_evidence(articles_df, genes_df, x),axis=1)
     annos_df['evidence_type'] = annos_df.apply(lambda x: get_evidence_type(x), axis=1)
-    annos_df = annos_df.drop(columns=['qualifiers'])
+    annos_df['groups'] = annos_df['evidence'].apply(lambda x: get_groups(x))
+    annos_df['evidence_count'] = annos_df['evidence'].apply(lambda x: count_evidence(x))
 
     return annos_df
 
