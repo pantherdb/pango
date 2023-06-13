b2cddeb5381bed190674bcb29a27ed0289a00346
tmushayahama <tmushayahama@gmail.com>
2023-06-13 14:51:18 -0700

    fix old site path

--- site-angular/scripts/update_site_nginx.sh
+++ site-angular/scripts/update_site_nginx.sh
@@ -3,5 +3,5 @@
 set -e
 
 date_suffix=$(date +'%m-%d-%y')
-mv /var/www/pango/site "old/site-$date_suffix"
+mv /var/www/pango/site "/var/www/pango/old/site-$date_suffix"
 mv /home/ubuntu/projects/pango/site-angular/dist/ /var/www/pango/site
