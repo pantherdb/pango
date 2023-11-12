aa06720e14edfce4b49fe0622ba3a5da6fbf6100
tmushayahama <tmushayahama@gmail.com>
2023-11-12 13:05:10 -0800

    changed the url to the api  https://github.com/pantherdb/pango/issues/45

--- site-angular/src/environments/environment.ts
+++ site-angular/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   amigoTermUrl: "http://amigo.geneontology.org/amigo/term/",
   pubmedUrl: "https://www.ncbi.nlm.nih.gov/pubmed/",
   taxonApiUrl: 'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=',
-  pangoGraphQLUrl: 'http://localhost:5000/graphql',
+  pangoGraphQLUrl: 'http://preview.functionome.org/api/graphql',
   ucscUrl: 'https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr',
   pantherFamilyUrl: 'https://www.pantherdb.org/treeViewer/treeViewer.jsp?',
   uniprotUrl: 'https://www.uniprot.org/uniprotkb/',
