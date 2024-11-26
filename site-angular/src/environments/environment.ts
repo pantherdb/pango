b271497ff9f66ec514eca5dc516401cac3dd7159
tmushayahama <tmushayahama@gmail.com>
2024-11-25 16:19:30 -0800

    ui for apiVersion param

--- site-angular/src/environments/environment.ts
+++ site-angular/src/environments/environment.ts
@@ -5,10 +5,11 @@
 export const environment = {
   production: false,
   annotationResultsSize: 50,
+  pangoGraphQLUrl: 'http://localhost:5000/graphql',
   amigoTermUrl: "http://amigo.geneontology.org/amigo/term/",
   pubmedUrl: "https://www.ncbi.nlm.nih.gov/pubmed/",
   taxonApiUrl: 'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=',
-  pangoGraphQLUrl: 'https://functionome.org/api/graphql',
+
   ucscUrl: 'https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr',
   pantherFamilyUrl: 'https://www.pantherdb.org/treeViewer/treeViewer.jsp?',
   overrepApiUrl: 'https://enrichment.functionome.org/webservices/go/overrep.jsp',
