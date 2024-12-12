63c45b159a02397de64125879b66a56780770a93
tmushayahama <tmushayahama@gmail.com>
2024-12-12 09:28:19 -0800

    Added the banner to the current version

--- site-angular/src/environments/environment.ts
+++ site-angular/src/environments/environment.ts
@@ -12,7 +12,6 @@ export const environment = {
   ucscUrl: 'https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr',
   pantherFamilyUrl: 'https://www.pantherdb.org/treeViewer/treeViewer.jsp?',
   overrepApiUrl: 'https://enrichment.functionome.org/webservices/go/overrep.jsp',
-  //overrepApiUrl: 'https://pantherdb.org/webservices/go/overrep.jsp',
 
   uniprotUrl: 'https://www.uniprot.org/uniprotkb/',
   agrPrefixUrl: 'https://www.alliancegenome.org/gene/',
